bd4c7b9c227d263d1745f75fb3580e45296427d8
Goforcereloaded <36819265+Goforcereloaded@users.noreply.github.com>
2025-04-29 02:00:40 +0200

    Update Microsoft Rewards.ps1

--- Microsoft Rewards.ps1
+++ Microsoft Rewards.ps1
@@ -172,17 +172,17 @@ function Type-Text($text) {
     $shell = New-Object -ComObject wscript.shell # Initialisation de l'objet shell
     $text.ToCharArray() | ForEach-Object {
         # Générer un délai aléatoire entre 50 et 70 ms avec une chance de 1%
-        # sinon entre 71 et 221 ms, et 1% de chance d'avoir entre 222 et 301 ms
+        # sinon entre 71 et 221 ms, et 1% de chance d'avoir entre 222 et 311 ms
         $randomValue = $random.Next(0, 100)
         if ($randomValue -lt 1) {
             $delay = $random.Next(50, 71)
         } elseif ($randomValue -ge 99) {
-            $delay = $random.Next(222, 302)
+            $delay = $random.Next(222, 312)
         } else {
             $delay = $random.Next(71, 222)
         }
         $shell.SendKeys($_)
-        Start-Sleep -Milliseconds $delay # Délai aléatoire entre 50 et 70 ms ou 71 et 221 ms ou 222 et 301 ms
+        Start-Sleep -Milliseconds $delay # Délai aléatoire entre 50 et 70 ms ou 71 et 221 ms ou 222 et 311 ms
     }
 }
 
@@ -227,8 +227,8 @@ for ($i = 0; $i -lt [math]::Min($shuffledWords1.Length, $shuffledWords2.Length);
     Type-Text $url
     $shell.SendKeys("{ENTER}") # Appuyer sur Entrée pour lancer la recherche
 
-    # Générer un délai aléatoire entre 5000 et 10001 millisecondes
-    $delay = $random.Next(5000, 10001)
+    # Générer un délai aléatoire entre 5000 et 11001 millisecondes
+    $delay = $random.Next(5000, 11001)
     # Attendre le délai spécifié
     Start-Sleep -Milliseconds $delay
 
